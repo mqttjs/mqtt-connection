ec2cafe68398b9df4b09b7a7d439e7c3f0b1dc6d
Matteo Collina <hello@matteocollina.com>
2014-12-16 18:44:19 +0100

    Initial import from MQTT.js and mqtt-packet.

---
+++ test/util.js
@@ -0,0 +1,12 @@
+
+var through = require('through2');
+
+module.exports.testStream = function() {
+  return through(function(buf, enc, cb) {
+    var that = this;
+    setImmediate(function() {
+      that.push(buf)
+      cb();
+    });
+  });
+};
